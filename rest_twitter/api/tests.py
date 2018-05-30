5eb7cbb3711c9041dcc26cb2bb39affa13031595
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2018-05-30 21:37:35 +0100

    add test for userprofile model

--- rest_twitter/api/tests.py
+++ rest_twitter/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import UserProfile
 
-# Create your tests here.
+
+class SecurityUserModelTest(TestCase):
+    """ Tests for the User profile Model """
+
+    def test_can_save_a_security_user(self):
+        UserProfile.objects.create(
+            email="sectest2@andela.com",
+            password="devpassword2",
+            name="NewPerson",
+        )
+        user_profile = UserProfile.objects.get(
+            name="NewPerson"
+        )
+
+        self.assertEqual(UserProfile.objects.count(), 1)
+        self.assertIn("sectest2@andela.com", user_profile.email)
